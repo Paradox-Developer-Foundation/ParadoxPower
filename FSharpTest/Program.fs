e4579c77b2ee5ab526f66bb447614ef3ba9a5c1b
textGamex <textGamex@outlook.com>
2024-12-30 21:57:31 +0800

    更新benchmark

--- FSharpTest/Program.fs
+++ FSharpTest/Program.fs
@@ -1,10 +1,75 @@
 ﻿module Program
 
+open ParadoxPower.CSharp
 open ParadoxPower.Parser
-open ParadoxPower.Process
-open ParadoxPower.Utilities.Position
 
-let node = Node("")
-let str = "Hello, world!"
-let leaf = Child.LeafC(Leaf("local_supplies", Value.Int(3), Range.Zero, Operator.Equals))
-node.SetValue str leaf
+
+let text = """
+state={
+        	id=1
+        	name="STATE_1" # Corsica
+        	manpower = 322900
+        	
+        	state_category = town
+
+        	history={
+        		owner = FRA
+        		victory_points = { 3838 1 }
+        		buildings = {
+        			infrastructure = 2
+        			industrial_complex = 1
+        			air_base = 1
+        			3838 = {
+        				naval_base = 3
+        			}
+        		}
+        		add_core_of = COR
+        		add_core_of = FRA
+        	}
+
+        	provinces={
+        		3838 9851 11804 
+        	}
+
+        	local_supplies=0.0 
+        }
+
+"""
+
+let text1 = """
+state={
+        	id=2
+        	name="STATE_21" # Corsica1
+        	manpower = 3229001
+        	
+        	state_category = town1
+
+        	history={
+        		owner = FRA1
+        		victory_points = { 3838 1 }
+        		buildings = {
+        			infrastructure = 1
+        			industrial_complex = 11
+        			air_base = 11
+        			3838 = {
+        				naval_base = 31
+        			}
+        		}
+        		add_core_of = COR1
+        		add_core_of = FRA1
+        	}
+
+        	provinces={
+        		3838 98511 118041 
+        	}
+
+        	local_supplies=0.0 
+        
+
+"""
+
+// let node = Parsers.ProcessStatements("123", "123",
+//         Parsers.ParseScriptFile("123.txt", text1).GetResult())
+
+let mess = Parsers.ParseScriptFile("123.txt", text1).GetError()
+printf $"%s{mess.ErrorMessage}"
--- ParadoxPower.Benchmark/Program.cs
+++ ParadoxPower.Benchmark/Program.cs
@@ -1,6 +1,10 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Buffers;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using FParsec;
+using Microsoft.FSharp.Core;
 using ParadoxPower.CSharp;
+using ParadoxPower.Localisation;
 using ParadoxPower.Parser;
 using ParadoxPower.Process;
 using ParadoxPower.Utilities;
@@ -11,85 +15,60 @@ namespace ParadoxPower.Benchmark;
 public class Program
 {
     private const string Text = """
-                                
-                                state={
-                                	id=607
-                                	name="STATE_607"
-                                	manpower=34289800
-                                	
-                                	state_category = town
-                                	
-                                	buildings_max_level_factor=1.000
-                                
-                                	history={
-                                		owner = CHI
-                                		add_core_of = CHI
-                                		add_core_of = PRC
-                                		buildings = {
-                                			infrastructure = 3 #was: 5
-                                			industrial_complex = 2
-                                		}
-                                		victory_points = {
-                                			9958 1 
-                                		}
-                                
-                                		1938.10.25 = {		
-                                			buildings = {
-                                				industrial_complex = 4
-                                				infrastructure = 4 #was: 8
-                                				air_base = 2
-                                				arms_factory = 2
-                                			}
-                                			JAP = {
-                                				set_province_controller = 1004
-                                				set_province_controller = 1139
-                                				set_province_controller = 4010
-                                				set_province_controller = 4114
-                                				set_province_controller = 4144
-                                				set_province_controller = 4606
-                                				set_province_controller = 6932
-                                				set_province_controller = 7074
-                                				set_province_controller = 7085
-                                				set_province_controller = 7129
-                                				set_province_controller = 9958
-                                				set_province_controller = 9995
-                                				set_province_controller = 10098
-                                				set_province_controller = 11931
-                                			} 			
-                                		}
-                                	}
-                                
-                                	provinces={
-                                		1004 1139 1603 4010 4066 4114 4144 4519 4547 4606 6932 7074 7085 7126 7129 7459 7508 7540 7568 7656 9958 9995 10098 10364 10434 10446 11931 12012 12426 
-                                	}
-                                
-                                	local_supplies=3.0 
-                                }
-                                """;
-    
+
+        state={
+        	id=1
+        	name="STATE_1" # Corsica
+        	manpower = 322900
+        	
+        	state_category = town
+
+        	history={
+        		owner = FRA
+        		victory_points = { 3838 1 }
+        		buildings = {
+        			infrastructure = 2
+        			industrial_complex = 1
+        			air_base = 1
+        			3838 = {
+        				naval_base = 3
+        			}
+        		}
+        		add_core_of = COR
+        		add_core_of = FRA
+        	}
+
+        	provinces={
+        		3838 9851 11804 
+        	}
+
+        	local_supplies=0.0 
+        }
+
+        """;
+
     private Node _node;
-    
+
+    // private readonly FSharpFunc<char, bool> isAnyOf = ;
+
     [GlobalSetup]
     public void Setup()
     {
-	    _node = Parsers.ProcessStatements("123", "123",
-		    Parsers.ParseScriptFile("123.txt", Text).GetResult()).Child("state").Value;
+        _node = Parsers.ProcessStatements("123", "123",
+            Parsers.ParseScriptFile("123.txt", Text).GetResult());
     }
-    
+
     public static void Main(string[] args)
     {
         BenchmarkRunner.Run<Program>();
     }
-    
-    [Benchmark]
-    public void Raw()
-    {
-        _node.SetValue("local_supplies", Child.NewLeafC(new Leaf("local_supplies", Types.Value.NewFloat(3), Position.Range.Zero, Types.Operator.Equals)));
-    }
+
+    private const string Comment = "# 12312312312";
+    private readonly CharStream<Unit> _stream = new(Comment, 0, Comment.Length);
 
     [Benchmark(Baseline = true)]
-    public void Optimized()
+    public object Raw()
     {
-	    _node.SetTagOpt("local_supplies", Child.NewLeafC(new Leaf("local_supplies", Types.Value.NewFloat(3), Position.Range.Zero, Types.Operator.Equals)));
+		return _node.ToRaw;
     }
 }
