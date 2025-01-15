b6da13fe3abc82e0dd9a84edd0b51a3fe2d9945f
textGamex <textGamex@outlook.com>
2025-01-15 23:53:05 +0800

    style: value 扩展方法添加文档

--- ParadoxPower.CSharpExtensions/ValueExtensions.cs
+++ ParadoxPower.CSharpExtensions/ValueExtensions.cs
@@ -6,6 +6,12 @@ namespace ParadoxPower.CSharpExtensions;
 
 public static class ValueExtensions
 {
+    /// <summary>
+    /// 尝试获取<c>int</c>
+    /// </summary>
+    /// <param name="val"></param>
+    /// <param name="value"></param>
+    /// <returns>当 <see cref="Types.Value"/> 为 <c>int</c> 时返回<c>true</c>, 反之返回<c>false</c></returns>
     public static bool TryGetInt(this Types.Value val, out int value)
     {
         if (val.IsInt)
@@ -17,6 +23,12 @@ public static class ValueExtensions
         return false;
     }
 
+    /// <summary>
+    /// 尝试获取<c>bool</c>
+    /// </summary>
+    /// <param name="val"></param>
+    /// <param name="value"></param>
+    /// <returns>当 <see cref="Types.Value"/> 为 <c>bool</c> 时返回<c>true</c>, 反之返回<c>false</c></returns>
     public static bool TryGetBool(this Types.Value val, out bool value)
     {
         if (val.IsBool)
@@ -29,6 +41,12 @@ public static class ValueExtensions
         return false;
     }
 
+    /// <summary>
+    /// 尝试获取<c>decimal</c>
+    /// </summary>
+    /// <param name="val"></param>
+    /// <param name="value"></param>
+    /// <returns>当 <see cref="Types.Value"/> 为 <c>decimal</c> 时返回<c>true</c>, 反之返回<c>false</c></returns>
     public static bool TryGetDecimal(this Types.Value val, out decimal value)
     {
         if (val.IsFloat)
@@ -41,6 +59,12 @@ public static class ValueExtensions
         return false;
     }
 
+    /// <summary>
+    /// 尝试获取<c>string</c>
+    /// </summary>
+    /// <param name="val"></param>
+    /// <param name="value"></param>
+    /// <returns>当 <see cref="Types.Value"/> 为 <c>string</c> 时返回<c>true</c>, 反之返回<c>false</c></returns>
     public static bool TryGetString(this Types.Value val, [NotNullWhen(true)] out string? value)
     {
         if (val.IsString)
@@ -53,6 +77,12 @@ public static class ValueExtensions
         return false;
     }
 
+    /// <summary>
+    /// 尝试获取带引号类型的<c>string</c>
+    /// </summary>
+    /// <param name="val"></param>
+    /// <param name="value"></param>
+    /// <returns>当 <see cref="Types.Value"/> 为带引号类型的 <c>string</c> 时返回<c>true</c>, 反之返回<c>false</c></returns>
     public static bool TryGetQString(this Types.Value val, [NotNullWhen(true)] out string? value)
     {
         if (val.IsQString)
@@ -64,19 +94,25 @@ public static class ValueExtensions
         value = null;
         return false;
     }
-
+    
+    /// <summary>
+    /// 尝试获取 <see cref="Types.Statement"/>
+    /// </summary>
+    /// <param name="val"></param>
+    /// <param name="value"></param>
+    /// <returns>当 <see cref="Types.Value"/> 为 <see cref="Types.Statement"/> 时返回<c>true</c>, 反之返回<c>false</c></returns>
     public static bool TryGetClause(
         this Types.Value val,
-        [NotNullWhen(true)] out IReadOnlyCollection<Types.Statement>? clause
+        [NotNullWhen(true)] out IReadOnlyCollection<Types.Statement>? value
     )
     {
         if (val.IsClause)
         {
-            clause = ((Types.Value.Clause)val).Item;
+            value = ((Types.Value.Clause)val).Item;
             return true;
         }
 
-        clause = null;
+        value = null;
         return false;
     }
 }
